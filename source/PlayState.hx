53dfa2f1a8554ca5740c8ddf1a6e35f9820d5af3
TheZoroForce240 <MZoroch@gmail.com>
2021-10-10 18:12:41 +0100

    hopefully fixed a thing with the camera

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -741,7 +741,7 @@ class PlayState extends MusicBeatState
 
 		add(camFollow);
 
-		FlxG.camera.follow(camFollow, LOCKON, 0.04 * (30 / (cast (Lib.current.getChildAt(0), Main)).getFPS()));
+		FlxG.camera.follow(camFollow, LOCKON, 0.04);
 		// FlxG.camera.setScrollBounds(0, FlxG.width, 0, FlxG.height);
 		FlxG.camera.zoom = defaultCamZoom;
 		FlxG.camera.focusOn(camFollow.getPosition());
